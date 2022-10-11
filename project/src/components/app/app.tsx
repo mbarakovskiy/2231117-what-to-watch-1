123f02e1bce04ac4b84743ff734f28850ee4bc06
Mikhail Barakovskiy <mbarakovskiy@gmail.com>
2022-10-11 18:01:51 +0500

    HW2

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,16 @@
+import MainPage from '../../pages/main/main';
+import {films} from '../../pages/main/films';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <MainPage
+      films={films}
+      limit={films.length}
+      name={'The Grand Budapest Hotel'}
+      genre={'Drama'}
+      year={2014}
+    />
+  );
 }
 
 export default App;
---
+++ project/src/pages/main/films.ts
@@ -0,0 +1,78 @@
+export const films = [
+  {
+    name: 'Fantastic Beasts: The Crimes of Grindelwald',
+    image: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg'
+  },
+  {
+    name: 'Bohemian Rhapsody',
+    image: 'img/bohemian-rhapsody.jpg'
+  },
+  {
+    name: 'Macbeth',
+    image: 'img/macbeth.jpg'
+  },
+  {
+    name: 'Aviator',
+    image: 'img/aviator.jpg'
+  },
+  {
+    name: 'We need to talk about Kevin',
+    image: 'img/we-need-to-talk-about-kevin.jpg'
+  },
+  {
+    name: 'What We Do in the Shadows',
+    image: 'img/what-we-do-in-the-shadows.jpg'
+  },
+  {
+    name: 'Revenant',
+    image: 'img/revenant.jpg'
+  },
+  {
+    name: 'Johnny English',
+    image: 'img/johnny-english.jpg'
+  },
+  {
+    name: 'Shutter Island',
+    image: 'img/shutter-island.jpg'
+  },
+  {
+    name: 'Pulp Fiction',
+    image: 'img/pulp-fiction.jpg'
+  },
+  {
+    name: 'No Country for Old Men',
+    image: 'img/no-country-for-old-men.jpg'
+  },
+  {
+    name: 'Snatch',
+    image: 'img/snatch.jpg'
+  },
+  {
+    name: 'Moonrise Kingdom',
+    image: 'img/moonrise-kingdom.jpg'
+  },
+  {
+    name: 'Seven Years in Tibet',
+    image: 'img/seven-years-in-tibet.jpg'
+  },
+  {
+    name: 'Midnight Special',
+    image: 'img/midnight-special.jpg'
+  },
+  {
+    name: 'War of the Worlds',
+    image: 'img/war-of-the-worlds.jpg'
+  },
+  {
+    name: 'Dardjeeling Limited',
+    image: 'img/dardjeeling-limited.jpg'
+  },
+  {
+    name: 'Mindhunter',
+    image: 'img/mindhunter.jpg'
+  },
+  {
+    name: 'Midnight Special',
+    image: 'img/midnight-special.jpg'
+  }
+];
