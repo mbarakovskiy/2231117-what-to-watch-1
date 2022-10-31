ae3f1bbf9b3b4de7e92c689c6ec04130d5cff6e2
Mikhail Barakovskiy <mbarakovskiy@gmail.com>
2022-10-31 22:36:02 +0500

    HW5.1

--- project/src/components/filmCard/film-card.tsx
+++ project/src/components/filmCard/film-card.tsx
@@ -1,4 +1,6 @@
 import {Film} from '../../types/film';
+import {useEffect, useState} from 'react';
+import VideoPlayer from '../video player/video-player';
 
 export type Props = {
   film: Film;
@@ -7,17 +9,43 @@ export type Props = {
 
 
 function FilmCard({film, onHover}: Props): JSX.Element {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const [isNeedVideoToPlay, setIsNeedVideoToPlay] = useState(false);
+
+  useEffect(() => {
+    let needUpdate = true;
+
+    if (isNeedVideoToPlay) {
+      setTimeout(() => needUpdate && setIsVideoPlaying(true), 1000);
+    }
+
+    return () => {needUpdate = false;};
+  }, [isNeedVideoToPlay]);
+
   return (
     <article
       className="small-film-card catalog__films-card"
-      onMouseEnter={() => onHover(film)}
-      onMouseLeave={() => onHover({} as Film)}
+      onMouseEnter={() => {
+        onHover(film);
+        setIsNeedVideoToPlay(true);
+      }}
+      onMouseLeave={() => {
+        onHover({} as Film);
+        setIsNeedVideoToPlay(false);
+        setIsVideoPlaying(false);
+      }}
     >
       <div className="small-film-card__image">
-        <img src={film.posterImage} alt={film.name} width="280" height="175"/>
+        <VideoPlayer
+          film={film}
+          isPlaying={isVideoPlaying}
+          muted
+          width={280}
+          height={175}
+        />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">{film.name}</a>
+        <a className="small-film-card__link" href="/">{film.name}</a>
       </h3>
     </article>
   );
---
+++ project/src/components/video player/video-player.tsx
@@ -0,0 +1,41 @@
+import {Film} from '../../types/film';
+import {useEffect, useRef} from 'react';
+
+type Props = {
+  film: Film;
+  isPlaying: boolean;
+  muted: boolean;
+  width: number;
+  height: number;
+}
+
+
+function VideoPlayer({film, isPlaying, muted, width, height}: Props): JSX.Element {
+  const videoPlayerRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (videoPlayerRef === null) {
+      return;
+    }
+
+    if (isPlaying) {
+      videoPlayerRef.current?.play();
+    } else {
+      videoPlayerRef.current?.load();
+    }
+
+  }, [isPlaying]);
+
+  return (
+    <video
+      ref={videoPlayerRef}
+      width={width}
+      height={height}
+      src={film.videoLink}
+      poster={film.posterImage}
+      muted={muted}
+    />
+  );
+}
+
+export default VideoPlayer;
